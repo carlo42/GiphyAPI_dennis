bcbcb3f36b508cf6e2f4837ca7d9bc4863eb514c
Carl Owen <carl.owen@hmhco.com>
2023-02-09 13:44:57 +0000

    wrapper

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -3,30 +3,25 @@ import { shallow } from 'enzyme';
 import Header from './Header.jsx';
 
 describe('<Header />', () => {
-  it('should have Puppies dropdown', () => {
-    const wrapper = shallow(
+  const getWrapper = filter => {
+    return shallow(
       <Header
         headerOptions={{
-          currentFilter: 'cute+puppies',
+          currentFilter: filter,
           disabled: false,
           offset: 0,
         }}
       />
     );
+  };
+  it('should have Puppies dropdown', () => {
+    const wrapper = getWrapper('cute+puppies');
     const actual = wrapper.find('DropdownButton').prop('title');
     const expected = 'Puppies';
     expect(actual).toEqual(expected);
   });
   it('should have Kittens dropdown', () => {
-    const wrapper = shallow(
-      <Header
-        headerOptions={{
-          currentFilter: 'cute+kittens',
-          disabled: false,
-          offset: 0,
-        }}
-      />
-    );
+    const wrapper = getWrapper('cute+kittens');
     const actual = wrapper.find('DropdownButton').prop('title');
     const expected = 'Kittens';
     expect(actual).toEqual(expected);
